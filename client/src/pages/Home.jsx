c37e6dd7fafb6379f0eb2ac56528b1f706794a77
Mukesh <mukesh@algogenix.com>
2024-08-01 14:55:04 +0530

    create header and add in root

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from 'flowbite-react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
