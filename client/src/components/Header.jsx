14462098453c58c30500c3854f28c02e8148e3be
Mukesh <mukesh@algogenix.com>
2024-08-06 14:10:20 +0530

    -create dark theme functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,22 @@
-import { Avatar, Button, Dropdown, DropdownHeader, Navbar, TextInput } from "flowbite-react";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownHeader,
+  Navbar,
+  TextInput,
+} from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const {theme} = useSelector(state=>state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -30,24 +40,31 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+         { theme==='light'?<FaSun/>:<FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown
-           arrowIcon={false}
-           inline
-           label = {<Avatar alt="user" img={currentUser.imageUrl} rounded/>}
+            arrowIcon={false}
+            inline
+            label={<Avatar alt="user" img={currentUser.imageUrl} rounded />}
           >
-           <Dropdown.Header>
+            <Dropdown.Header>
               <span className=" block text-sm">@{currentUser.username}</span>
-              <span className=" block text-sm truncate font-medium">{currentUser.email}</span>
-           </Dropdown.Header>
-           <Link to='/dashboard?tab=profile'>
-           <Dropdown.Item>Profile</Dropdown.Item>
-           </Link>
-           <Dropdown.Divider/>
-           <Dropdown.Item>Sign Out</Dropdown.Item>
+              <span className=" block text-sm truncate font-medium">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to="/dashboard?tab=profile">
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in">
