a5b3be07558b3d2c0a49eb88a4b6e750c5971b9d
Mukesh <mukesh@algogenix.com>
2024-08-08 06:08:13 +0530

    frontend -create dashboard side bar

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import {Sidebar} from 'flowbite-react'
+import {HiUser, HiArrowSmRight} from 'react-icons/hi'
+
+export default function DashSidebar() {
+  
+  const location = useLocation();
+  const [tab,setTab] = useState("");
+  useEffect(()=>{
+    const searchUrl = new URLSearchParams(location.search);
+    const fetchTab = searchUrl.get('tab');
+    if(fetchTab && fetchTab!=""){
+      setTab(fetchTab);
+    }
+  },[location.search]);
+  return (
+    <Sidebar className=' w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={'/dashboard?tab=profile'}>
+          <Sidebar.Item active={tab==='profile'} icon={HiUser} label="User" labelColor='dark'>Profile</Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashProfile from '../components/DashProfile'
+import DashSidebar from '../components/DashSidebar';
+import {useLocation} from 'react-router-dom'
 
 export default function Dashboard() {
+
+  const location = useLocation();
+  const [tab,setTab] = useState("");
+  useEffect(()=>{
+    const searchUrl = new URLSearchParams(location.search);
+    const fetchTab = searchUrl.get('tab');
+    if(fetchTab && fetchTab!=""){
+      setTab(fetchTab);
+    }
+  },[location.search]);
+
   return (
-    <div>Dashboard</div>
+    <div className=' min-h-screen flex flex-col md:flex-row'>
+      {/* dash board slider */}
+      <div className=' min-w-56'>
+        <DashSidebar/>
+      </div>
+      {/* profilea and other */}
+     {tab==='profile' && <DashProfile/>}
+    </div>
   )
 }
